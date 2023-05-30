8ea58e3409a3bfc2fca39b39a973093d68c17345
Maliha-Zaman <malihazaman@iut-dhaka.edu>
2023-05-30 16:47:39 +0600

    appointment

--- myappointments.php
+++ myappointments.php
@@ -1,67 +1,87 @@
 <?php
 
-  $host = "localhost";
-  $dbUsername = "root";
-  $dbPassword = "";
-  $dbname = "findadoc";
-  
-  $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname = "findadoc";
 
-  if (mysqli_connect_error())
-  {
-    die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-  }
+$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
 
-  session_start();
-  $email = $_SESSION['patientemail'];
-  $name = $_SESSION['patientname'];
+if (mysqli_connect_error()) {
+  die('Connect Error(' . mysqli_connect_errno() . ')' . mysqli_connect_error());
+}
+
+session_start();
+$email = $_SESSION['patientemail'];
+$name = $_SESSION['patientname'];
 ?>
 
 <html>
-  <head>
-      <title>
-    
-      </title>
-      <link rel = "stylesheet" >
-  </head>
-  <body>
-    
-  
-      
-      <h1>My Appointments</h1>
+
+<head>
+  <title>
+
+  </title>
+  <link rel="stylesheet">
+</head>
+
+<body>
+
+
+
+
+  <?php
+  $query = "SELECT * FROM requests WHERE pname = '$name'and frommail = '$email' AND status = 'a'";
+  $result = mysqli_query($conn, $query);
+
+  ?>
+  <div class="container">
+    <h1>My Appointments</h1>
+    <table>
+      <thead>
+        <tr>
+
+          <th>Appointment Date</th>
+          <th>Appointment time</th>
+          <th>Doctor Contact</th>
+
+        </tr>
+      </thead>
+      <tbody>
         <?php
-          $query = "SELECT * FROM requests WHERE pname = '$name'and frommail = '$email' AND status = 'a'";
-          $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_array($result)) {
+          $requester = $row['tomail'];
+          //echo $requester;
+        
+          $query2 = "SELECT pname, date, time, tomail FROM requests WHERE tomail = '$requester' and frommail = '$email' AND status = 'a' ";
+          $result2 = mysqli_query($conn, $query2);
 
-          while($row = mysqli_fetch_array($result))
-          {
-            $requester = $row['tomail'];
-            //echo $requester;
-
-            $query2 = "SELECT pname, date, time, tomail FROM requests WHERE tomail = '$requester' and frommail = '$email' AND status = 'a' ";
-            $result2 = mysqli_query($conn, $query2);
-            
-    if (!$result2) {
-        // Handle the error
-        echo "Error: " . mysqli_error($conn);
-      } else{
+          if (!$result2) {
+            // Handle the error
+            echo "Error: " . mysqli_error($conn);
+          } else {
             $patientinfo = mysqli_fetch_array($result2);
-         ?>
-                  <div class = "info">
-                  <!-- <label id = "institution" style="font-weight:bold;">Patient Name</label> <label style="margin-left:9px; margin-right:4px; font-weight:bold;">:</label> <label> <?php echo $patientinfo['pname'];?></label><br> -->
-                  <label id = "institution" style="font-weight:bold;">Appointment Date</label> <label style="margin-left:9px; margin-right:4px; font-weight:bold;">:</label> <label> <?php echo $patientinfo['date'];?></label><br>
-                  <label id = "availability" style="font-weight:bold;">Appointment Time</label> <label style="margin-left:5px; margin-right:4px; font-weight:bold;">:</label> <label><?php echo $patientinfo['time'];?></label><br>
-                  <label id = "availability" style="font-weight:bold;">Doctor Contact</label> <label style="margin-left:5px; margin-right:4px; font-weight:bold;">:</label> <label><?php echo $patientinfo['tomail'];?></label><br>
-                  
+            ?>
+            <tr>
 
-                  
-                  </div>
-          <?php  
+              <td class="amount">
+                <?php echo $patientinfo['date']; ?>
+              </td>
+              <td>
+                <?php echo $patientinfo['time']; ?>
+              </td>
+              <td>
+                <?php echo $patientinfo['tomail']; ?>
+              </td>
+            </tr>
+            <?php
           }
         }
         ?>
-      </div>
-        </div>
-  </body>
+      </tbody>
+    </table>
+  </div>
+
+</body>
 
 </html>
