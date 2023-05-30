274e8a44b87dbb58db3dff1482ca1140f4559c14
shaisad <95139737+shaisad@users.noreply.github.com>
2023-05-31 05:33:27 +0600

    constraint for timing clash added

--- sendbookrequest.php
+++ sendbookrequest.php
@@ -1,6 +1,4 @@
 <?php
-
-
 $host = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
@@ -12,7 +10,6 @@ $tomail = $_POST['tomail'];
 $date = $_POST['date'];
 $time = $_POST['time'];
 
-
 session_start();
 $frommail = $_SESSION['patientemail'];
 $pname = $_SESSION['patientname'];
@@ -21,36 +18,44 @@ if (mysqli_connect_error()) {
   die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
 }
 
+// Check if the appointment already exists and is accepted
+$checkQuery = "SELECT * FROM requests WHERE tomail = '$tomail' AND date = '$date' AND time = '$time' AND status = 'Accepted'";
+$checkResult = mysqli_query($conn, $checkQuery);
 
+if (mysqli_num_rows($checkResult) > 0) {
+    // The appointment already exists and is accepted
+ 
 
-if(!empty($tomail) && !empty($date) && !empty($time)) {
-    $q = "SELECT * FROM requests WHERE frommail = '$frommail' AND tomail = '$tomail' and date ='$date' and time='$time'";
-    $res = mysqli_query($conn, $q);
+    ?>
+    <script>alert("The appointment for the selected day and time has already been accepted. Please choose a different time!")</script>
+    <?php
+    exit();
+} else {
+    // The appointment doesn't exist or is not accepted, proceed with sending the request
+    if (!empty($tomail) && !empty($date) && !empty($time)) {
+        $q = "SELECT * FROM requests WHERE frommail = '$frommail' AND tomail = '$tomail' and date ='$date' and time='$time'";
+        $res = mysqli_query($conn, $q);
 
-    if(mysqli_num_rows($res) > 0) {
-        $num_rows = mysqli_num_rows($res);
-        echo "Number of rows: " . $num_rows;
-        echo "request already sent";
-        exit();
-    }
-    else {
-        $query = "INSERT INTO requests (frommail, tomail, status, date, time, pname) values ('$frommail', '$tomail', 'Pending', '$date', '$time', '$pname')";
-        $result = mysqli_query($conn, $query);
-        //$requestId = mysqli_insert_id($conn);
-  
-        if($result) {
-            ?>      
-            <script>alert("Request sent successfully!")</script>
-
+        if (mysqli_num_rows($res) > 0) {
+            $num_rows = mysqli_num_rows($res);
+            ?>
+            <script>alert("Request already sent!")</script>
             <?php
+            
             exit();
-        }
-        else {
-            echo "Error";
+        } else {
+            $query = "INSERT INTO requests (frommail, tomail, status, date, time, pname) values ('$frommail', '$tomail', 'Pending', '$date', '$time', '$pname')";
+            $result = mysqli_query($conn, $query);
+
+            if ($result) {
+                ?>
+                <script>alert("Request sent successfully!")</script>
+                <?php
+                exit();
+            } else {
+                echo "Error";
+            }
         }
     }
 }
 ?>
-
-
-
