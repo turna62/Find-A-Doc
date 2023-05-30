67622233d67693e17699ee3adf12247ccb8390c9
shaisad <95139737+shaisad@users.noreply.github.com>
2023-05-31 02:21:03 +0600

    constraint of not reviewing oneself added

--- submitdoctorreview.php
+++ submitdoctorreview.php
@@ -4,31 +4,40 @@ session_start();
 $review = $_POST['review'];
 $rpatientname = $_SESSION['patientname'];
 
-
 require 'dbConfig.php'; 
 
 if (!empty($review)){
     if(isset($_POST["submitreview"])){
         $sno = $_GET['docid'];
 
-        // Check if the user has already reviewed this doctor
-        $checkQuery = "SELECT COUNT(*) as count FROM reviews WHERE rpatientname = '$rpatientname' AND rdoctorid = '$sno'";
-        $checkResult = mysqli_query($db, $checkQuery);
-        $checkData = mysqli_fetch_assoc($checkResult);
+        // Check if the user is reviewing themselves
+        $doctorQuery = "SELECT doctorid FROM doctor WHERE doctorid = '$sno' AND doctorname = '$rpatientname'";
+        $doctorResult = mysqli_query($db, $doctorQuery);
+        $doctorData = mysqli_fetch_assoc($doctorResult);
 
-        if ($checkData['count'] > 0) {
-            // User has already reviewed this doctor
-            echo '<script>alert("You have already reviewed this doctor!")</script>';
+        if ($doctorData) {
+            // User is reviewing themselves
+            echo '<script>alert("You cannot review yourself as a doctor!")</script>';
         } else {
-            // Insert the new review
-            $INSERT = "INSERT INTO `reviews` (`rpatientname`, `review`, `rdoctorid`) VALUES ('$rpatientname', '$review', '$sno')";
-        
-            if(mysqli_query($db, $INSERT))
-            {
-                echo '<script>alert("Review Successful!")</script>';
-            }
-            else {
-                echo '<script>alert("Error while submitting review! Please try again!")</script>';
+            // Check if the user has already reviewed this doctor
+            $checkQuery = "SELECT COUNT(*) as count FROM reviews WHERE rpatientname = '$rpatientname' AND rdoctorid = '$sno'";
+            $checkResult = mysqli_query($db, $checkQuery);
+            $checkData = mysqli_fetch_assoc($checkResult);
+
+            if ($checkData['count'] > 0) {
+                // User has already reviewed this doctor
+                echo '<script>alert("You have already reviewed this doctor!")</script>';
+            } else {
+                // Insert the new review
+                $INSERT = "INSERT INTO `reviews` (`rpatientname`, `review`, `rdoctorid`) VALUES ('$rpatientname', '$review', '$sno')";
+            
+                if(mysqli_query($db, $INSERT))
+                {
+                    echo '<script>alert("Review Successful!")</script>';
+                }
+                else {
+                    echo '<script>alert("Error while submitting review! Please try again!")</script>';
+                }
             }
         }
     }
